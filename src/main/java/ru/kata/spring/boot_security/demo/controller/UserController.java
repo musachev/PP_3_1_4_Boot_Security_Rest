a01fdc7c498af08d19f1e15ebb3fdf0c471886aa
fatherice <fatherice@gmail.com>
2022-05-31 16:07:48 +0300

    First Commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,76 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import expert.usachev.web.pp_3_1_1_SpringBoot.model.User;
+import expert.usachev.web.pp_3_1_1_SpringBoot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "hello";
+    }
+
+    @ModelAttribute("newUser")
+    public User getPerson() {
+        return new User();
+    }
+
+    @GetMapping("/people")
+    public String index(Model model) {
+        model.addAttribute("people", userService.getAllUsers());
+        return "view/index";
+    }
+
+    @PostMapping("/people")
+    public String creat(@ModelAttribute("newUser") User user,
+                        BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("people", userService.getAllUsers());
+            return "view/index";
+        }
+        userService.saveUser(user);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/people/{id}")
+    public String deletePerson(@PathVariable("id") int id) {
+        userService.removeUserById(id);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/people/{id}/edit")
+    public String edit(@ModelAttribute("id") int id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "view/edit";
+    }
+
+    @PatchMapping("/people/{id}")
+    public String updatePerson(@ModelAttribute("user") User updateuser, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "view/edit";
+        }
+        userService.updateUser(updateuser);
+        return "redirect:/people";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDAO.java
@@ -0,0 +1,43 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import expert.usachev.web.pp_3_1_1_SpringBoot.model.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+
+@Repository
+public class UserDAO {
+    @PersistenceContext()
+    private EntityManager entityManager;
+
+    @Transactional
+    public void saveUser(User user) {
+        entityManager.persist(user);
+        entityManager.close();
+    }
+
+    @Transactional
+    public void updateUser(User updateUser) {
+        entityManager.merge(updateUser);
+    }
+
+    @Transactional
+    public void removeUserById(int id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("SELECT user FROM User user", User.class).getResultList();
+    }
+
+    public User getUserById(int id) {
+        return entityManager.find(User.class, id);
+    }
+}
